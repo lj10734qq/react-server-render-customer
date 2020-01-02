0b48fdcafbf207df1dd1d8d33860059b567f633e
linjian <linjian@szkingdom.com>
2020-01-02 15:10:25 +0800

    提交初次渲染代码demo

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: linjian
+ * @Date: 2020-01-02 11:23:04
+ * @LastEditors: linjian
+ * @Description: file content
+ * @email: linjian@szkingdom.com
+ */
+export default () => <h1>this is first server render!</h1>
